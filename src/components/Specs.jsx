856092a3924d2295fc632be2ae1274f71e07a70b
TairSadyk <tairsadykov92@gmail.com>
2022-07-03 16:52:45 -0400

    final version

--- src/components/Specs.jsx
+++ src/components/Specs.jsx
@@ -3,21 +3,21 @@ import React from "react";
 function Specs() {
   return (
     <div className="specs">
-      <div className="price">
+      <div className="price specs__content">
         <img
-          className="price__icon"
+          className="specs__icon specs__icon--eth"
           src="img/icon-ethereum.svg"
           alt="Ethereum icon"
         />
-        <p className="price__text">0.041 ETH</p>
+        <p className="specs__text specs__text--eth">0.041 ETH</p>
       </div>
-      <div className="days-left">
+      <div className="specs__content">
         <img
-          className="days-left__icon"
+          className="specs__icon specs__icon--time"
           src="img/icon-clock.svg"
           alt="Clock icon"
         />
-        <p className="days-left__text">3 days left</p>
+        <p className="specs__text specs__text--time">3 days left</p>
       </div>
     </div>
   );
